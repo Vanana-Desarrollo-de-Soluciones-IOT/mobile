f88c7e2ce035e6d351943cbd9350d2e6ca515b98
zGIKS <166082577+zGIKS@users.noreply.github.com>
2026-05-26 00:36:24 -0500

    Add shell.nix for Android SDK and Flutter development environment

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {
+    config = {
+      android_sdk.accept_license = true;
+      allowUnfree = true;
+    };
+  }
+}:
+
+let
+  buildToolsVersion = "34.0.0";
+  androidComposition = pkgs.androidenv.composeAndroidPackages {
+    buildToolsVersions = [ buildToolsVersion "28.0.3" ];
+    platformVersions = [ "36" "34" "28" ];
+    abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+  };
+  androidSdk = androidComposition.androidsdk;
+in
+pkgs.mkShell {
+  ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+  
+  buildInputs = with pkgs; [
+    flutter
+    androidSdk
+    jdk17
+  ];
+}
